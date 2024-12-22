9f3328826bc8552e4bf1f09c245167f93e5db9c8
Ahmed Faraj <148560615+Ahmad-Faraj@users.noreply.github.com>
2024-12-22 05:24:56 +0200

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,4 +1,4 @@
-let pieces = [
+var pieces = [
   " ",
   "♔",
   "♕",
@@ -14,58 +14,58 @@ let pieces = [
   "♟",
 ];
 
-let moves = [king, queen, rook, bishop, knight, pawn];
-let pieceNum = moves.length;
+var moves = [king, queen, rook, bishop, knight, pawn];
+var pieceNum = 6;
 
-let initialState = {
+var initialState = {
   turn: 0,
   board: [1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 12, 11, 10, 9, 8, 7],
 };
 
 function canMove(state, from, to) {
-  let pFrom = state.board[from];
+  var pFrom = state.board[from];
   if (pFrom == 0) return false;
 
-  let p = owner(pFrom);
+  var p = owner(pFrom);
   if (state.turn != p) return false;
 
-  let pTo = state.board[to];
+  var pTo = state.board[to];
   if (pTo != 0 && owner(pTo) == p) return false; // There is no castling ( We don't need castling anyway right ? )
 
   return moves[piece(pFrom)](state, from, to);
 }
 
 function rook(state, from, to) {
-  let smaller = Math.min(from, to);
-  let bigger = Math.max(from, to);
-  let board = state.board;
-  for (let i = smaller + 1; i < bigger; ++i) {
+  var smaller = Math.min(from, to);
+  var bigger = Math.max(from, to);
+  var board = state.board;
+  for (var i = smaller + 1; i < bigger; ++i) {
     if (board[i] != 0) return false;
   }
   return true;
 }
 
 function bishop(state, from, to) {
-  let smaller = Math.min(from, to);
-  let bigger = Math.max(from, to);
-  let board = state.board;
-  for (let i = smaller + 2; i < bigger; i = i + 2) {
+  var smaller = Math.min(from, to);
+  var bigger = Math.max(from, to);
+  var board = state.board;
+  for (var i = smaller + 2; i < bigger; i = i + 2) {
     if (i < board.length && board[i] != 0) return false;
   }
   return i == bigger;
 }
 
 function queen(state, from, to) {
-  return rook(state, from, to) || bishop(state, from, to);
+  return rook(state, from, to) || bishop(state, from, to) ;
 }
 
 function knight(state, from, to) {
-  let diff = Math.abs(to - from);
+  var diff = Math.abs(to - from);
   return diff == 2 || diff == 3;
 }
 
 function pawn(state, from, to) {
-  let dir = state.turn == 0 ? 1 : -1;
+  var dir = state.turn == 0 ? 1 : -1;
   return (
     from + dir == to ||
     (initialState.board[from] == state.board[from] &&
@@ -75,7 +75,7 @@ function pawn(state, from, to) {
 }
 
 function king(state, from, to) {
-  let diff = Math.abs(to - from);
+  var diff = Math.abs(to - from);
   return diff == 1;
 }
 
@@ -84,7 +84,7 @@ function owner(p) {
 }
 
 function piece(p) {
-  let q = p - 1;
+  var q = p - 1;
   return q < pieceNum ? q : q - pieceNum;
 }
 
@@ -96,33 +96,33 @@ function decodeNum(xs, i) {
 }
 
 function encodeState(state) {
-  let board = state.board;
-  let result = encodeNum("", state.turn);
-  for (let i = 0; i < board.length; ++i) {
+  var board = state.board;
+  var result = encodeNum("", state.turn);
+  for (var i = 0; i < board.length; ++i) {
     result = encodeNum(result, board[i]);
   }
   return result;
 }
 
 function decodeState(b) {
-  let board = [];
-  let state = {};
+  var board = [];
+  var state = {};
   state.turn = decodeNum(b, 0);
   state.board = board;
-  for (let i = 1; i < b.length; ++i) {
+  for (var i = 1; i < b.length; ++i) {
     board[i - 1] = decodeNum(b, i);
   }
   return state;
 }
 
 function getState() {
-  let x = new URL(window.location).searchParams.get("state");
+  var x = new URL(window.location).searchParams.get("state");
   return !x ? initialState : decodeState(x);
 }
 
 function moving(state, from) {
   return function () {
-    let opts = [];
+    var opts = [];
     for (let to = 0; to < state.board.length; ++to) {
       let td = document.getElementById("b" + to);
       let list = td.classList;
@@ -151,10 +151,10 @@ function moving(state, from) {
 }
 
 function threatened(state, p) {
-  let res = false;
-  let t = state.turn;
+  var res = false;
+  var t = state.turn;
   state.turn = 1 - t;
-  for (let i = 0; i < state.board.length; ++i) {
+  for (var i = 0; i < state.board.length; ++i) {
     res = canMove(state, i, p);
     if (res) break;
   }
@@ -163,17 +163,17 @@ function threatened(state, p) {
 }
 
 function drawState(state) {
-  let table = document.createElement("table");
+  var table = document.createElement("table");
   table.classList.add("board");
-  let row = document.createElement("tr");
+  var row = document.createElement("tr");
   table.appendChild(row);
 
-  let board = state.board;
+  var board = state.board;
 
-  for (let i = 0; i < board.length; ++i) {
-    let td = document.createElement("td");
+  for (var i = 0; i < board.length; ++i) {
+    var td = document.createElement("td");
     td.classList.add(i % 2 == 0 ? "b" : "w");
-    let p = board[i];
+    var p = board[i];
     td.textContent = pieces[p];
     td.setAttribute("id", "b" + i);
     if (owner(p) == state.turn) {
@@ -186,13 +186,13 @@ function drawState(state) {
     row.appendChild(td);
   }
 
-  let note = document.createElement("div");
+  var note = document.createElement("div");
   note.textContent = "Current state: " + encodeState(state);
 
-  let turn = document.createElement("div");
+  var turn = document.createElement("div");
   turn.textContent = (state.turn == 0 ? "White" : "Black") + "'s move:";
 
-  let body = document.getElementById("main");
+  var body = document.getElementById("main");
   body.appendChild(note);
   body.appendChild(turn);
   body.appendChild(table);
